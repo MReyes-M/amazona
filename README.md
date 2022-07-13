d811f0715a6cfc9c1905423201bd422c812b166c
MReyes-M <mario_reyes@dialog.com.au>
2022-07-13 11:07:56 +1000

    Up to lesson 10

--- README.md
+++ README.md
@@ -2,5 +2,65 @@
 
 #
 
-1.Create React App
-2.Create Git repo
+1. Create React App
+
+2. Create Git repo
+
+3. List Products
+   a. Create products array
+   b. add product images
+   c. render products
+   d. style products.
+
+4. Add routing
+   a.npm i react-router-dom
+   b. create route for home screen
+   c. create route for product screen.
+
+5. Create Node.js server
+   a. run npm init in root folder
+   b. update package.json set type: module
+   c. add .js to imports
+   d. npm install express
+   e. create route for / return backend is ready
+   f. move products.js from frontend to backend
+   g. create route for /api/products
+   h. return products
+   i. run npm start
+
+6. Fetch Products from backend
+   a. Set proxy in package,json
+   b. npm install axios
+   c. use state hook
+   d. use effect hook
+   e. use reducer
+
+7. Manage state by reducer hook
+   a. define reducer
+   b. update fetch data
+   c. get state from usReducer
+
+8. Add bootstrap UI fwk
+   a. npm install react-bootstrap
+   b. update app.js
+
+9. Create product and rating component
+   a. create rating component
+   b. create product component
+   c. use rating component in product component
+
+10. Create product details screen
+    a. fetch product from backend
+    b. create 3 columns for image info and action
+
+11. Create loading and message component
+    a. create loading component
+    b. use spinner component
+    c. create message component
+    d. create utils.js to define getError function
+
+12. Implement add to cart
+    a. create React Context
+    b. define a reducer
+    c. create store provider
+    d. implement add to cart button click handler
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,61 @@
+import { useEffect, useReducer } from "react";
+import axios from "axios";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Product from "../Components/Product";
+import { Helmet } from "react-helmet-async";
+import LoadingBox from "../Components/LoadingBox";
+import MessageBox from "../Components/MessageBox";
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'FETCH_REQUEST':
+            return { ...state, loading: true }
+        case 'FETCH_SUCCESS':
+            return { ...state, products: action.payload, loading: false }
+        case 'FETCH_FAIL':
+            return { ...state, error: action.payload, loading: false }
+        default:
+            return state;
+    }
+}
+
+function HomeScreen() {
+    const [{ loading, error, products }, dispatch] = useReducer(reducer, { loading: true, error: '', products: [] });
+    useEffect(() => {
+        const fetchData = async () => {
+            dispatch({ type: 'FETCH_REQUEST' });
+            try {
+                const result = await axios.get('/api/products');
+                dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+            } catch (error) {
+                dispatch({ type: 'FETCH_FAIL', payload: error.message });
+            }
+        }
+        fetchData();
+    }, []);
+    return <div>
+        <Helmet>
+            <title>Amazona</title>
+        </Helmet>
+        <h1>Featured Products</h1>
+        <div className="products">
+            {
+                loading ? (<div><LoadingBox></LoadingBox></div>
+                ) : error ? (<div><MessageBox variant="danger">{error}</MessageBox></div>)
+                    :
+                    <Row>
+                        {
+                            products.map(product => (
+                                <Col sm={6} md={4} lg={3} className="mb-3" key={product.slug}>
+                                    <Product product={product}></Product>
+                                </Col>
+                            ))
+                        }
+                    </Row>
+
+            }
+        </div></div>
+}
+
+export default HomeScreen;
